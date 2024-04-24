cc773f7adedcc9e3365fc1830683d3a5248c5078
Luiz Felipe <luizfelipesiqueira83@gmail.com>
2024-04-24 15:17:34 -0300

    Iniciando mais um desafio de lógica
    Obtendo os valores do select

---
+++ carrinho-compras/js/app.js
@@ -0,0 +1,14 @@
+function adicionar(){
+    let select = document.querySelector('.produto-input');
+    let valorselect = select.value;
+    console.log(valorselect);
+
+    let valores = [];
+
+
+}
+
+
+
+
+function limpar(){}
