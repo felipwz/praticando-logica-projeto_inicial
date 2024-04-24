352878a51ebbf16258a9ade36fc0e0498beb3513
Luiz Felipe <luizfelipesiqueira83@gmail.com>
2024-04-24 16:58:49 -0300

    Desafio finalizado

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,6 +1,5 @@
 let totalGeral = 0;
-document.getElementById('lista-produtos').innerHTML = '';
-document.getElementById('valor-total').textContent = 'R$ 0';
+limpar();
 
 
 function adicionar() {
@@ -9,26 +8,32 @@ function adicionar() {
     let nomeProduto = produto.split('-')[0];
     let valorProduto = produto.split('R$')[1];
     let quantidade = document.getElementById('quantidade').value;
-    let preco = quantidade * valorProduto;
-    totalGeral = totalGeral + preco;
-    
 
-
-    let carrinho = document.getElementById('lista-produtos');
-    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+    if (quantidade >= 1) {
+        let preco = quantidade * valorProduto;
+        totalGeral = totalGeral + preco;
+        let carrinho = document.getElementById('lista-produtos');
+        carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
     <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${valorProduto}</span></section>`;
 
-    let campoTotal = document.getElementById('valor-total');
-    campoTotal.textContent = `R$ ${totalGeral}`
+        let campoTotal = document.getElementById('valor-total');
+        campoTotal.textContent = `R$ ${totalGeral}`
 
-    quantidade = document.getElementById('quantidade').value = '';
-
-
-
+        quantidade = document.getElementById('quantidade').value = '';
+    } else {
+        alert('É necessário selecionar uma quantidade do produto para adicioná lo ao seu carrinho.')
+    }
 
 }
 
 
 
 
-function limpar() {}
+function limpar() {
+    totalGeral = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$ 0';
+
+
+
+}
