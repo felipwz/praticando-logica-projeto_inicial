d72a493fa6648a3cd4310f3a9c21e462c54cb9cc
Luiz Felipe <luizfelipesiqueira83@gmail.com>
2024-04-24 16:39:54 -0300

    Mais funções

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,12 +1,34 @@
-function adicionar(){
-    let nomeProduto = document.getElementById('produto');
-    let quantidade = document.getElementById('quantidade');
-    alert(nomeProduto.value);
-    alert(quantidade.value);
+let totalGeral = 0;
+document.getElementById('lista-produtos').innerHTML = '';
+document.getElementById('valor-total').textContent = 'R$ 0';
+
+
+function adicionar() {
+    // recuperar valores, nome do produto e quantidade
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorProduto = produto.split('R$')[1];
+    let quantidade = document.getElementById('quantidade').value;
+    let preco = quantidade * valorProduto;
+    totalGeral = totalGeral + preco;
+    
+
+
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${valorProduto}</span></section>`;
+
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$ ${totalGeral}`
+
+    quantidade = document.getElementById('quantidade').value = '';
+
+
+
 
 }
 
 
 
 
-function limpar(){}
+function limpar() {}
