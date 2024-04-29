492bf11fc167e2e8ae87130aaf754b887b72ec3f
felipwz <luizfelipesiqueira83@gmail.com>
2024-04-29 20:12:53 -0300

    Agora está certo

--- ingresso/js/app.js
+++ ingresso/js/app.js
@@ -1,27 +1,74 @@
 function comprar() {
-    let tipo = document.getElementById('tipo-ingresso');
-    let qtd = parseInt(document.getElementById('qtd')).value;
+    let tipo = document.getElementById('tipo-ingresso').value;
+    let qtd = parseInt(document.getElementById('qtd').value)
 
-    if (tipo.value == 'pista') {
+
+    if (tipo == 'inferior') {
+        comprarInferior(qtd);
+    }
+
+    if (tipo == 'superior') {
+        comprarSuperior(qtd);
+    }
+
+    if (tipo == 'pista') {
         comprarPista(qtd);
     }
 }
 
+function comprarInferior(qtd) {
+    let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+    console.log(qtd);
+
+    if (qtd > qtdInferior) {
+        alert('Os ingressos para pista esgotaram..')
+
+    } else {
+
+        qtdInferior = qtdInferior - qtd;
+        qtdInferior.textContent = qtdInferior;
+        alert('Compra realizada com sucesso!');
+        document.getElementById('qtd-inferior').textContent = qtdInferior;
+
+    }
+}
+
+
+function comprarSuperior(qtd) {
+    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+    console.log(qtd);
+
+    if (qtd > qtdSuperior) {
+        alert('Os ingressos para pista esgotaram..')
+
+    } else {
+
+        qtdSuperior = qtdSuperior - qtd;
+        qtdSuperior.textContent = qtdSuperior;
+        alert('Compra realizada com sucesso!');
+        document.getElementById('qtd-superior').textContent = qtdSuperior;
+
+    }
+
+}
+
+
+
 function comprarPista(qtd) {
-
     let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+    console.log(qtd);
 
-        if (qtd > qtdPista) {
-            alert('Os ingressos para pista esgotaram..')
-            
-        } else{
-            
-            qtdPista = qtdPista - qtd;
-            qtdPista.textContent = qtdPista;
-            document.getElementById('qtd-pista').textContent = qtdPista;
-            alert('Compra realizada com sucesso!');   
+    if (qtd > qtdPista) {
+        alert('Os ingressos para pista esgotaram..')
 
-        }
+    } else {
 
-    
+        qtdPista = qtdPista - qtd;
+        qtdPista.textContent = qtdPista;
+        alert('Compra realizada com sucesso!');
+        document.getElementById('qtd-pista').textContent = qtdPista;
+
+    }
+
+
 }
