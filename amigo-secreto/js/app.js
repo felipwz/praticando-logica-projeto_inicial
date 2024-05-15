ac6e830d85e534f3fa98ac166f48336bf08613f1
Luiz Felipe <luizfelipesiqueira83@gmail.com>
2024-05-15 14:57:57 -0300

    último desafio
    a

--- amigo-secreto/js/app.js
+++ amigo-secreto/js/app.js
@@ -1,43 +1,53 @@
 let amigos = [];
 
 function adicionar() {
-    let amigo = document.getElementById('nome-amigo');
-    let lista = document.getElementById('lista-amigos');
-    amigos.push(amigo.value);
-
-    if (lista.textContent == '') {
-        lista.textContent = amigo.value;
-    } else {
-        lista.textContent = lista.textContent + ', ' + amigo.value
+    let amigo = document.getElementById("nome-amigo");
+    if (amigo.value == "") {
+        alert("Por favor digite o nome das pessoas que iram participar do amigo secreto");
+        return;
     }
 
-    amigo.value = '';
+    if (amigos.includes(amigo.value)) {
+        alert('Nome já adicionado');
+        return;
 
+    }
+
+
+    let lista = document.getElementById("lista-amigos");
+    if (lista.textContent == "") {
+        lista.textContent = amigo.value;
+    } else {
+        lista.textContent = lista.textContent + ", " + amigo.value;
+    }
+
+    amigos.push(amigo.value);
+    amigo.value = "";
 }
 
 function sortear() {
-    embaralha(amigos);
 
-    let sorteio = document.getElementById('lista-sorteio');
-
-    for (let i = 0; i < amigos.length; i++) {
-
-        if (i == amigos.length - 1) {
-            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' ---> ' + amigos[0] + '<br>';
-        } else {
-            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' ---> ' + amigos[i + 1] + '<br>';
-        }
-
-
+    if (amigos.length < 4) {
+        alert('É preciso colocar no minimo 4 pessoas para o sorteio');
+        return;
     }
 
 
+    embaralha(amigos);
+    let sorteio = document.getElementById("lista-sorteio");
+    for (let i = 0; i < amigos.length; i++) {
+        if (i == amigos.length - 1) {
+            sorteio.innerHTML =
+                sorteio.innerHTML + amigos[i] + " ---> " + amigos[0] + "<br>";
+        } else {
+            sorteio.innerHTML =
+                sorteio.innerHTML + amigos[i] + " ---> " + amigos[i + 1] + "<br>";
+        }
+    }
 }
 
 function embaralha(lista) {
-
     for (let indice = lista.length; indice; indice--) {
-
         const indiceAleatorio = Math.floor(Math.random() * indice);
 
         // guarda de um índice aleatório da lista
@@ -47,13 +57,10 @@ function embaralha(lista) {
 
         lista[indiceAleatorio] = elemento;
     }
-
 }
 
 function reiniciar() {
     amigos = [];
-    let lista = document.getElementById('lista-amigos').innerHTML = '';
-    let sorteio = document.getElementById('lista-sorteio').innerHTML = '';
-   
-
+    let lista = (document.getElementById("lista-amigos").innerHTML = "");
+    let sorteio = (document.getElementById("lista-sorteio").innerHTML = "");
 }
